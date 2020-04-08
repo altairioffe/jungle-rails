c62a66d4c9227a1aea0552525311bd34cbf046cd
Altair <altair.ioffe@gmail.com>
2020-04-08 06:43:05 +0000

    added capybara test for add to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js:true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+
+      @category.products.create!(
+        name:  'shirt',
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    
+
+  end
+
+  scenario "cart is empty on page load" do
+    visit root_path
+    # puts. root_path.html
+    # find("a[href='/products/1']").click
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+
+    expect(page).to have_content 'My Cart (0)'
+  end
+
+  scenario "Clicking add to cart increases cart by 1" do
+    visit root_path
+    click_on "Add"
+    # puts. root_path.html
+    # find("a[href='/products/1']").click
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+
+    expect(page).to have_content 'My Cart (1)'
+  end
+
+end
